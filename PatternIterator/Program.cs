86a59968000ecfd2e6a2672fff7a8efd6d55e187
Ali MAKRI <microsoft@makri.fr>
2021-05-21 08:53:15 +0200

    08:53

---
+++ PatternIterator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Personne { Nom = "Gérard", Ville = "Grenoble" };
+            var p2 = new Personne { Nom = "Héloïse", Ville = "Honfleur" };
+            List<Personne> ps = new List<Personne> { p1, p2 };
+
+            // Personne à l'index 0
+            var p0 = ps[0];
+
+            // Liste
+            foreach(var p in ps)
+            {
+                Console.WriteLine(p);
+            }
+
+            Console.ReadLine();
+        }
+    }
+    class Personne
+    {
+        public string Nom;
+        public string Ville;
+    }
+}
