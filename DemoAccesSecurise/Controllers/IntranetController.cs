90e9f6d8ac3a338484a298b02b43464ea8cb04b7
Ali MAKRI <microsoft@makri.fr>
2021-06-01 13:41:10 +0200

    13:41

--- DemoAccesSecurise/Controllers/IntranetController.cs
+++ DemoAccesSecurise/Controllers/IntranetController.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace DemoAccesSecurise.Controllers
 {
     [Authorize]
     public class IntranetController : Controller
     {
+        public IntranetController()
+        {
+            if (!Roles.RoleExists("Admin")) Roles.CreateRole("Admin");
+            if (!Roles.IsUserInRole("ali.makri@gmail.fr", "Admin")) Roles.AddUserToRole("ali.makri@gmail.fr", "Admin");
+            if (!Roles.IsUserInRole("paul@gmail.fr", "Admin")) Roles.AddUserToRole("paul@gmail.fr", "Admin");
+        }
         [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Employe()
+        {
+            return View();
+        }
+
+        [Authorize(Roles ="Admin")]
+        public ActionResult Admin()
         {
             return View();
         }
---
+++ DemoInjectionSql/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoInjectionSql.MvcApplication" Language="C#" %>
